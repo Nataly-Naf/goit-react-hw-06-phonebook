e75cd06a241156580b905b184a87a9024d344bc4
Nataly-Naf <nat.spivak84@gmail.com>
2023-11-04 17:47:33 +0000

    Update FilterForm.js

--- src/components/FilterForm/FilterForm.js
+++ src/components/FilterForm/FilterForm.js
@@ -1,20 +1,30 @@
-import { useEffect, useState } from "react";
-import { FilterInput } from "./FilterForm.styled"
-import { useDispatch, useSelector} from 'react-redux';
-import { setFilter } from "redux/contactsSlice";
-import { getFilter } from "redux/selectors";
+import { useEffect, useState } from 'react';
+import { FilterInput } from './FilterForm.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from 'redux/contactsSlice';
+import { getFilter } from 'redux/selectors';
 
 export const FilterForm = () => {
-    const [value, setValue] = useState('');
-    useEffect(()=>{dispatch(setFilter(value))}, [value])
+  const [value, setValue] = useState('');
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-      const savedFilter = useSelector(getFilter);
+  useEffect(() => {
+    dispatch(setFilter(value));
+  }, [value, dispatch]);
 
-    return (
-        <div>
-            <p>Find contact by name</p>
-            <FilterInput onChange={event => {setValue(event.target.value);  }} type="text" placeholder="Find by name" value={savedFilter} />
-        </div>
-    )
-}
+  const savedFilter = useSelector(getFilter);
+
+  return (
+    <div>
+      <p>Find contact by name</p>
+      <FilterInput
+        onChange={event => {
+          setValue(event.target.value);
+        }}
+        type="text"
+        placeholder="Find by name"
+        value={savedFilter}
+      />
+    </div>
+  );
+};
